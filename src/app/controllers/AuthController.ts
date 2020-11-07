a7cd2425d467f4f419f2e4cc0b88e684cc54bf05
WatanabeThiago <watanabe.thiago4@gmail.com>
2020-11-07 12:14:12 -0300

    Rota de Login

--- src/app/controllers/AuthController.ts
+++ src/app/controllers/AuthController.ts
@@ -7,39 +7,35 @@ import User from '../models/User'
 
 class AuthController {
     async login(req: Request, res: Response) {
-        const repository = getRepository(User);
+        const repository = getRepository(User);                                         // Receber o repositório do model User
 
-        const { user_email, user_password } = req.body;
+        const { user_email, user_password } = req.body;                                 // Receber dados do front/mobile.
         console.log(req.body);
-        const user = await repository.findOne({ where: { user_email } })
+        const user = await repository.findOne({ where: { user_email } })                // Procurar pelo user_email no banco de dados.
         
-        if(!user)
+        if(!user)                                                                       // Verificar se esse email não existe no banco de dados.
         {
-            res.sendStatus(401)
+            res.sendStatus(401)                                                         // Senão existe, retornar status 401.
         }
         
         
-        const isValidPassword = await bcrypt.compare(user_password, user.user_password)
+        const isValidPassword = await bcrypt.compare(user_password, user.user_password) // Comparar a senha do usuario com a senha do banco de dados.
         console.log("Comparado")
-        if(!isValidPassword)
+        if(!isValidPassword)                                                            // Se a senha estiver errada, retornar "Senha errada"
         {
             return res.json("Senha errada")
         }
         console.log("isValidPassword")
-        const token = jwt.sign({id: user.user_id}, 'secret', {expiresIn: '1d'})
+        const token = jwt.sign({id: user.user_id}, 'secret', {expiresIn: '1d'})         // Gerar token de autenticação
 
-        return res.json({
+        return res.json({                                                               // Retornar usuario
             user,
             token
         })
 
     }
     
-
-
 }
 
 
-
-
-export default new AuthController();
+export default new AuthController();                                                    // Exportar rota.
